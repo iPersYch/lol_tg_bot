2ddb68c96cfcbe37500c7fef5ce3d9e28919371d
iPers <steacherok94@gmail.com>
2022-12-05 14:10:43 +0300

    v.1.0.0
    - Первая итерация

---
+++ main.py
@@ -0,0 +1,110 @@
+import pickle
+from time import sleep
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.utils import executor
+import requests
+
+with open('database/config.pickle', 'rb') as f:
+    config_info = pickle.load(f)
+bot = Bot(token=config_info['token'])
+api_key = config_info['api_key']
+
+# For example use simple MemoryStorage for Dispatcher.
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+
+# States
+class Form(StatesGroup):
+    user_SummonerName = State()
+    user_Server = State()
+
+
+@dp.message_handler(commands='start')
+async def cmd_start(message: types.Message):
+    """
+    Conversation's entry point
+    """
+    # Set state
+    await Form.user_SummonerName.set()
+    try:
+        with open('database/users_info.pickle', 'rb') as f:
+            users_info = pickle.load(f)
+    except:
+        users_info = {}
+    if message.from_user.id not in users_info.keys():  # Проверяем есть ли ИД в БД
+        await message.reply("Привет, отправь мне свой ник в League of Legends")
+
+        @dp.message_handler(state='*', commands='cancel')
+        @dp.message_handler(Text(equals='cancel', ignore_case=True), state='*')
+        async def cancel_handler(message: types.Message, state: FSMContext):
+            current_state = await state.get_state()
+            if current_state is None:
+                return
+            await state.finish()
+            # And remove keyboard (just in case)
+            await message.reply('Cancelled.', reply_markup=types.ReplyKeyboardRemove())
+
+        @dp.message_handler(state=Form.user_SummonerName)
+        async def process_name(message: types.Message, state: FSMContext):
+            """
+            Process user name
+            """
+            async with state.proxy() as data:
+                data['user_SummonerName'] = message.text
+
+            await Form.next()
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+            markup.add("RU", "EU", "NA")
+            await message.reply("What is your server", reply_markup=markup)
+
+        # Check age. Age gotta be digit
+        @dp.message_handler(state=Form.user_Server)
+        async def process_age(message: types.Message, state: FSMContext):
+            # Update state and data
+            await Form.next()
+            await state.update_data(user_Server=message.text)
+            markup = types.ReplyKeyboardRemove()
+
+            async with state.proxy() as data:
+                response = requests.get(
+                    f"https://ru.api.riotgames.com/lol/summoner/v4/summoners/by-name/{data['user_SummonerName']}",
+                    params={"api_key": api_key})
+                users_info[message.from_user.id] = {"id": response.json()['id'],
+                                                    "accountId": response.json()['accountId'],
+                                                    'name': response.json()['name'],
+                                                    "summonerLevel": response.json()['summonerLevel'],
+                                                    'Server': f'{data["user_Server"]}',
+                                                    'puuid': response.json()['accountId'],
+                                                    "profileIconId": response.json()['profileIconId']}
+                with open('database/users_info.pickle', 'wb') as f:
+                    pickle.dump(users_info, f)
+                await bot.send_message(message.chat.id,
+                                       'Теперь вы в нашей Базе Данных, введите /start ,\nчтобы убедиться',
+                                       reply_markup=markup)
+
+                await state.finish()
+    else:
+        await bot.send_message(message.chat.id,
+                               f'Добро пожаловать призыватель, Мы Вас помним!\nВаше Имя призывателя: {users_info[message.from_user.id]["name"]}\nСервер: {users_info[message.from_user.id]["Server"]}\nУровень призывателя: {users_info[message.from_user.id]["summonerLevel"]}\nВаш ID призывателя: {users_info[message.from_user.id]["id"]}'
+                               f'<a href="http://ddragon.leagueoflegends.com/cdn/12.22.1/img/profileicon/{users_info[message.from_user.id]["profileIconId"]}.png">&#8205</a>',
+                               parse_mode='HTML',disable_web_page_preview=False)
+
+        # while True:
+        #     sleep(900)
+        #     response1 = requests.get(f"https://ru.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/pSUEMC0Samkn_nZIiO1BD9xIsLEcHFLTrsDBevLnfG6XaD4",params={"api_key":api_key})
+        #     if response1.status_code == 200:
+        #         await bot.send_message(message.chat.id, "ALARM!! СЕРЕЖА В ИГРЕ")
+
+    # with open('database/phrase_dict.pickle', 'rb') as f:
+    #     phrase_dict = pickle.load(f)
+    # with open('database/phrase_dict.pickle', 'wb') as f:
+    #     pickle.dump(phrase_dict, f)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
